f45c62a1a97e4b3e3f3cdec92a48955a546c476c
ASHOK SASITHARAN <asasitharan28@gmail.com>
2025-07-03 18:03:19 -0400

    Changed self description

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -26,11 +26,11 @@ export default function Home() {
       </div>
 
       <div className="m-20 text-m/6" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
-        <p>Hi, I’m Ashok — a passionate Full Stack Software Developer with a focus on building clean, efficient, and user-friendly applications. I specialize in full-stack development, front-end design and backend systems, and I love turning ideas into reality through code.
-          With a background in computer science, I’ve worked on a variety of projects ranging from small personal tools to large-scale applications / web apps. I enjoy tackling new challenges, learning emerging technologies, and continuously improving both my code and my problem-solving approach.
-          My toolkit includes technologies like Ruby on Rails, PostgreSQL JavaScript, Docker, React, Node.js, Python and SQL, and I’m always open to experimenting with new tools that help bring better solutions to life.
-          Outside of coding, you’ll usually find me with my camera looking for awesome photo spots.
-          Whether you're here to explore my work or looking for a collaborator, I’d love to connect!
+        <p>Hi, Im Ashok — a passionate Full Stack Software Developer with a focus on building clean, efficient, and user-friendly applications. I specialize in full-stack development, front-end design and backend systems, and I love turning ideas into reality through code.
+          With a background in computer science, Ive worked on a variety of projects ranging from small personal tools to large-scale applications / web apps. I enjoy tackling new challenges, learning emerging technologies, and continuously improving both my code and my problem-solving approach.
+          My toolkit includes technologies like Ruby on Rails, PostgreSQL JavaScript, Docker, React, Node.js, Python and SQL, and Im always open to experimenting with new tools that help bring better solutions to life.
+          Outside of coding, youll usually find me with my camera looking for awesome photo spots.
+          Whether you're here to explore my work or looking for a collaborator, Id love to connect!
         </p>
       </div>
 
