f90620df8f2fc46895737def1ce08dcf4b08b944
ASHOK SASITHARAN <asasitharan28@gmail.com>
2025-07-03 16:14:08 -0400

    Fixed match media compile issue

--- blocks/Components/Masonry/Masonry.tsx
+++ blocks/Components/Masonry/Masonry.tsx
@@ -17,17 +17,21 @@ const useMedia = (
   values: number[],
   defaultValue: number,
 ): number => {
+  // @ts-ignore
+  if (typeof window === "undefined" || !window.matchMedia) return null;
   const get = () =>
-    values[queries.findIndex((q) => matchMedia(q).matches)] ?? defaultValue;
+
+    values[queries.findIndex((q) => window.matchMedia(q).matches)] ?? defaultValue;
 
   const [value, setValue] = useState<number>(get);
+  
 
   useEffect(() => {
     const handler = () => setValue(get);
-    queries.forEach((q) => matchMedia(q).addEventListener("change", handler));
+    queries.forEach((q) => window.matchMedia(q).addEventListener("change", handler));
     return () =>
       queries.forEach((q) =>
-        matchMedia(q).removeEventListener("change", handler),
+        window.matchMedia(q).removeEventListener("change", handler),
       );
   }, [queries]);
 
