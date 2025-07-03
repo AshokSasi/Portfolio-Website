2add1efd2b7f2dd31c695ad781bd6ef12120f61d
ASHOK SASITHARAN <asasitharan28@gmail.com>
2025-07-03 03:13:52 -0400

    Fixed Dock error

--- blocks/Components/Dock/Dock.tsx
+++ blocks/Components/Dock/Dock.tsx
@@ -91,13 +91,13 @@ function DockItem({
       onFocus={() => isHovered.set(1)}
       onBlur={() => isHovered.set(0)}
       onClick={onClick}
-      className={` relative inline-flex items-center justify-center rounded-full bg-[#060010] border-neutral-700 border-2 shadow-md ${className}`}
+      className={`relative inline-flex items-center justify-center rounded-full bg-[#060010] border-neutral-700 border-2 shadow-md ${className}`}
       tabIndex={0}
       role="button"
       aria-haspopup="true"
     >
       {Children.map(children, (child) =>
-        cloneElement(child as React.ReactElement, { isHovered }),
+        cloneElement(child as any, { isHovered }),
       )}
     </motion.div>
   );
